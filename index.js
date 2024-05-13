cdd103ba42980c50b1b6f749c3ac6bd9f4b9cf20
Mahmudjon Shukurov <sukurovmahmudzon15@gmail.com>
2024-05-13 09:41:06 +0300

    tugadi

---
+++ index.js
@@ -0,0 +1,11 @@
+let sonda = Number(prompt("Hafta kunini sonda kiriting!"));
+
+switch (sonda) {
+    case 1: alert('dushanba'); break;
+    case 2: alert('seshanba');break;
+    case 3: alert('chorshanba');break;
+    case 4: alert('payshanba');break;
+    case 5: alert('juma');break;
+    case 6: alert('shanba');break;
+    case 7: alert('yakshanba');break;
+}
